2135e3d1a49fa565c22374bf9281d37dc97e326a
Wohlstand <admin@wohlnet.ru>
2023-08-29 05:57:16 +0300

    load_file.cpp: Exteded the debugging code

--- test/load_file.cpp
+++ test/load_file.cpp
@@ -1,65 +1,72 @@
 #include <stdio.h>
 #include <FreeImageLite.h>
 
+static void s_fiDebug(FREE_IMAGE_FORMAT fif, const char *msg)
+{
+	fprintf(stderr, "FI <%u>: %s\n", (uint32_t)fif, msg);
+	fflush(stderr);
+}
+
 // strangely undocumented import necessary to use the FreeImage handle functions
 extern void SetDefaultIO(FreeImageIO *io);
 
 int main(int argc, char** argv)
 {
-    int ret = 0;
+	int ret = 0;
 
-    if(argc < 2)
-    {
-        fprintf(stderr, "Syntax: %s <path to file>\n", argv[0]);
-        fflush(stdout);
-        return 1;
-    }
+	if(argc < 2)
+	{
+		fprintf(stderr, "Syntax: %s <path to file>\n", argv[0]);
+		fflush(stderr);
+		return 1;
+	}
 
-    FILE *file = fopen(argv[1], "rb");
-    if(!file)
-    {
-        fprintf(stderr, "fopen: Can't open file %s\n", argv[1]);
-        fflush(stdout);
-        return 1;
-    }
+	FILE *file = fopen(argv[1], "rb");
+	if(!file)
+	{
+		fprintf(stderr, "fopen: Can't open file %s\n", argv[1]);
+		fflush(stderr);
+		return 1;
+	}
 
-    FreeImage_Initialise();
+	FreeImage_Initialise();
+	FreeImage_SetOutputMessage(&s_fiDebug);
 
-    FreeImageIO io;
-    SetDefaultIO(&io);
+	FreeImageIO io;
+	SetDefaultIO(&io);
 
-    FREE_IMAGE_FORMAT formato = FreeImage_GetFileTypeFromHandle(&io, (fi_handle)file);
+	FREE_IMAGE_FORMAT formato = FreeImage_GetFileTypeFromHandle(&io, (fi_handle)file);
 
-    if(formato == FIF_UNKNOWN)
-    {
-        fclose(file);
-        fprintf(stderr, "FreeImage_GetFileTypeFromHandle: Can't recognise format %s\n", argv[1]);
-        fflush(stdout);
-        FreeImage_DeInitialise();
-        return 0;
-    }
+	if(formato == FIF_UNKNOWN)
+	{
+		fclose(file);
+		fprintf(stderr, "FreeImage_GetFileTypeFromHandle: Can't recognise format %s\n", argv[1]);
+		fflush(stderr);
+		FreeImage_DeInitialise();
+		return 0;
+	}
 
-    FIBITMAP *img = FreeImage_LoadFromHandle(formato, &io, (fi_handle)file);
-    fclose(file);
-    if(img)
-    {
-        fprintf(stderr, "Successfully loaded image: %s\n", argv[1]);
-        fprintf(stderr, "W = %u\n", FreeImage_GetWidth(img));
-        fprintf(stderr, "H = %u\n", FreeImage_GetHeight(img));
-        fprintf(stderr, "B = %u\n", FreeImage_GetBPP(img));
-        fflush(stdout);
+	FIBITMAP *img = FreeImage_LoadFromHandle(formato, &io, (fi_handle)file);
+	fclose(file);
+	if(img)
+	{
+		fprintf(stderr, "Successfully loaded image: %s\n", argv[1]);
+		fprintf(stderr, "W = %u\n", FreeImage_GetWidth(img));
+		fprintf(stderr, "H = %u\n", FreeImage_GetHeight(img));
+		fprintf(stderr, "B = %u\n", FreeImage_GetBPP(img));
+		fflush(stderr);
 
-        FreeImage_Unload(img);
-        ret = 0;
-    }
-    else
-    {
-        fprintf(stderr, "FreeImage_LoadFromHandle: Can't load image file %s\n", argv[1]);
-        fflush(stdout);
-        ret = 1;
-    }
+		FreeImage_Unload(img);
+		ret = 0;
+	}
+	else
+	{
+		fprintf(stderr, "FreeImage_LoadFromHandle: Can't load image file %s\n", argv[1]);
+		fflush(stderr);
+		ret = 1;
+	}
 
-    FreeImage_DeInitialise();
+	FreeImage_DeInitialise();
 
-    return ret;
+	return ret;
 }
